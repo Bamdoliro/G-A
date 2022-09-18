17c8bf3df79ec0784cea14e04857249c9a34ee4d
unknown <sj01048260279@gmail.com>
2022-09-18 13:43:43 +0900

    ADD :: ChatListSection - Scroll View 추가해서 Scroll 되게 함

--- pages/ChatScreen.js
+++ pages/ChatScreen.js
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 import ChatList from "../components/ChatList/ChatList";
-import ChatListData from "../data/ChatList.json"
+import ChatListData from "../data/ChatList.json";
 
 export default function ChatScreen() {
     return (
