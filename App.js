29ef43b0ce9e698e7413a302c937c32ec614562f
unknown <sj01048260279@gmail.com>
2022-09-20 22:23:10 +0900

    ADD :: ChatLiveScreen App 에 연결

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import LoginScreen from "./pages/LoginScreen";
 import ChatScreen from "./pages/ChatScreen";
+import ChatLiveScreen from "./pages/ChatLiveScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -16,7 +17,8 @@ export default function App() {
         }}
       >
         {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
-        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        {/* <Stack.Screen name="ChatScreen" component={ChatScreen} /> */}
+        <Stack.Screen name="ChatLiveScreen" component={ChatLiveScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
