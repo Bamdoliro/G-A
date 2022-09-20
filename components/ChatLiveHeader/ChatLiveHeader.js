16ed2d0dd07f26712658eaf73cd2410061add494
unknown <sj01048260279@gmail.com>
2022-09-20 22:26:27 +0900

    ADD :: ChatLiveHeader 생성

---
+++ components/ChatLiveHeader/ChatLiveHeader.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function ChatLiveHeader() {
+    return (
+        <View>
+
+        </View>
+    )
+}
--- pages/ChatLiveScreen.js
+++ pages/ChatLiveScreen.js
@@ -1,9 +1,10 @@
 import { View } from "react-native";
+import ChatLiveHeader from "../components/ChatLiveHeader/ChatLiveHeader";
 
 export default function ChatLiveScreen() {
     return (
         <View>
-
+            <ChatLiveHeader />
         </View>
     )
 }
