ed84938ec21b630e1943b1d5d55f197e4829177b
unknown <sj01048260279@gmail.com>
2022-09-21 20:38:41 +0900

    ADD :: ChatLiveSendFeild 생성

---
+++ components/ChatLiveSendFeild/ChatLiveSendFeild.js
@@ -0,0 +1,9 @@
+import { View, StyleSheet } from "react-native";
+
+export default function ChatLiveSendFeild() {
+    return (
+        <View>
+
+        </View>
+    )
+}
--- pages/ChatLiveScreen.js
+++ pages/ChatLiveScreen.js
@@ -1,11 +1,12 @@
 import { View, StyleSheet } from "react-native";
 import ChatLiveHeader from "../components/ChatLiveHeader/ChatLiveHeader";
-import ChatLiveFeild from "../components/ChatLiveFeild/ChatLiveFeild";
+import ChatLiveSendFeild from "../components/ChatLiveSendFeild/ChatLiveSendFeild";
 
 export default function ChatLiveScreen() {
     return (
         <View style={styles.ChatLiveFeild} >
             <ChatLiveHeader />
+            <ChatLiveSendFeild />
         </View>
     )
 }
