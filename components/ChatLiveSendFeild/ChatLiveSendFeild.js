7963b742e6ace4ad235ad8d81580c98cbf9736a2
unknown <sj01048260279@gmail.com>
2022-09-22 20:29:09 +0900

    ADD :: ChatLiveSendFeild 부분 완성 - 이벤트들 추가 안함 이벤트 추가해야함

--- components/ChatLiveSendFeild/ChatLiveSendFeild.js
+++ components/ChatLiveSendFeild/ChatLiveSendFeild.js
@@ -1,9 +1,43 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput, Image, TouchableOpacity } from "react-native";
+import SendBtn from "../../assets/SendBtn.png";
 
 export default function ChatLiveSendFeild() {
     return (
-        <View>
-
+        <View style={styles.ChatSendInputView}>
+            <TextInput 
+                style={styles.ChatSendInput}
+            />
+            <TouchableOpacity>
+                <Image 
+                    source={SendBtn}
+                    style={styles.ChatSendBtn}
+                />
+            </TouchableOpacity>           
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    ChatSendInputView: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    ChatSendInput: {
+        backgroundColor: "#fff",
+        width: "80%",
+        fontSize: 15,
+        borderRadius: 15,
+        marginRight: 20,
+        padding: 5,
+        backgroundColor: "#E7E7E7",
+        outLineStyle: "none",
+        outlineStyle: "none",
+        paddingLeft: 15
+    } ,
+    ChatSendBtn: {
+        height: "30px",
+        width: "30px"
+    }
+
+})
--- pages/ChatLiveScreen.js
+++ pages/ChatLiveScreen.js
@@ -5,15 +5,30 @@ import ChatLiveSendFeild from "../components/ChatLiveSendFeild/ChatLiveSendFeild
 export default function ChatLiveScreen() {
     return (
         <View style={styles.ChatLiveFeild} >
-            <ChatLiveHeader />
-            <ChatLiveSendFeild />
+            <ChatLiveHeader style={{flex:1 , backgroundColor:"blue"}}/>
+            <View style={styles.ChatSendInputView}>
+                <ChatLiveSendFeild style={styles.ChatLiveSendFeild} />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     ChatLiveFeild: {
+        flex: 1,
         backgroundColor: "#D4E0FF",
-        height: "100%"
-    }
+    },
+    ChatLiveSendFeild: {
+        flex: 1,
+        backgroundColor: "#fff",
+        height: 10
+    },
+    ChatSendInputView: {
+        flex: 1,
+        position: "fixed",
+        bottom: 0,
+        padding: 10,
+        width: "100%",
+        backgroundColor: "#fff",     
+    },
 })
