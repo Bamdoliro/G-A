8aad50913e5f563705941239b85e611e07b792b1
gimhanul <gimhanuly@gmail.com>
2022-09-27 05:39:01 +0900

    ADD :: 야매 로그인
    - 액세스 토큰 넣어서 로그인 할 수있게 ㅎㅎ...
    - 급해서 쩔수없어용

---
+++ api/urls.js
@@ -0,0 +1,2 @@
+export const baseUrl = "http://3.39.41.34";
+export const socketUrl = "3.39.41.34:9090";
--- pages/LoginScreen.js
+++ pages/LoginScreen.js
@@ -1,65 +1,88 @@
-import React, { useState } from "react";
-import { Button, Image, StyleSheet, View } from "react-native";
+import React, {useState} from "react";
+import {Image, StyleSheet, View} from "react-native";
 import CustomInput from "../components/CustomInput/CustomInput";
 import Logo from "../assets/img.png";
 import SafeAreaView from "../components/SafeAreaView/SafeAreaView";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import CustomButton from "../components/CustomButton/CustomButton";
 
-export default function LoginScreen() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default function LoginScreen({navigation}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  return (
-    <SafeAreaView style={styles.root}>
-      <View style={styles.innerBox}>
-        <Image source={Logo} style={styles.logo} resizeMode="contain" />
-        <CustomInput
-          placeholder="이메일 입력"
-          value={email}
-          setValue={setEmail}
-        />
-        <CustomInput
-          placeholder="비밀번호 입력"
-          value={password}
-          setValue={setPassword}
-          secureTextEntry={true}
-        />
-      </View>
-    </SafeAreaView>
-  );
+    const loginUser = async () => {
+        try {
+            await AsyncStorage.setItem("access-token", email); // ㅎㅎ 나중에 수정하세욤
+            // socket io client connect and subscribe
+            navigation.navigate("ChatScreen");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.root}>
+            <View style={styles.innerBox}>
+                <Image source={Logo} style={styles.logo} resizeMode="contain"/>
+                <CustomInput
+                    placeholder="이메일 입력"
+                    value={email}
+                    setValue={setEmail}
+                />
+                <CustomInput
+                    placeholder="비밀번호 입력"
+                    value={password}
+                    setValue={setPassword}
+                    secureTextEntry={true}
+                />
+                <View style={styles.loginButton}>
+                    <CustomButton
+                        title="로그인"
+                        onPress={loginUser}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  avoidingView: {
-    flex: 1,
-  },
-  innerBox: {
-    height: "70%",
-    width: "84%",
-
-    backgroundColor: "white",
-    borderRadius: 30,
-
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    avoidingView: {
+        flex: 1,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    innerBox: {
+        height: "70%",
+        width: "84%",
 
-    alignItems: "center",
-    justifyContent: "center",
-  },
+        backgroundColor: "white",
+        borderRadius: 30,
 
-  logo: {
-    bottom: "5%",
-    width: "100%",
-    height: "46%",
-  },
-  root: {
-    flex: 1,
-    backgroundColor: "#0D76FF",
-    justifyContent: "center",
-    alignItems: "center",
-  },
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    loginButton: {
+        width: "90%",
+        height: 30,
+        alignItems: "center",
+    },
+
+    logo: {
+        bottom: "5%",
+        width: "100%",
+        height: "46%",
+    },
+    root: {
+        flex: 1,
+        backgroundColor: "#0D76FF",
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
