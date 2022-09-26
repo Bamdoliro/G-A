fbacd979ee67547da66de95eda7750357180f90e
unknown <sj01048260279@gmail.com>
2022-09-26 18:11:49 +0900

    ADD :: 오류 수정

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import LoginScreen from "./pages/LoginScreen";
-import ChatScreen from "./pages/ChatScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginScreen from './pages/LoginScreen';
+import TabNavigation from './TabNavigation';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      <Stack.Navigator initialRouteName="TabNavigation"
         screenOptions={{
           headerShown: false
         }}
       >
-        {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
-        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        <Stack.Screen 
+          name="TabNavigation"
+          component={TabNavigation}
+        />
+        <Stack.Screen 
+          name="LoginScreen"
+          component={LoginScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
