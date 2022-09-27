b75390ffc81d2ef5e772a50fd9ff3994c6db5782
SEOKKAMONI <sj01048260279@gmail.com>
2022-09-28 01:15:47 +0900

    ADD :: FlatList 추가 MessageData 수정 map -> FlatList 변경

--- components/ChatLiveFeild/ChatLiveFeild.js
+++ components/ChatLiveFeild/ChatLiveFeild.js
@@ -1,36 +1,43 @@
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, Text, FlatList, View } from "react-native";
 import ChatLiveMyMessage from "../ChatLiveMessage/ChatLiveMyMessage";
 import ChatLiveOtherMessage from "../ChatLiveMessage/ChatLiveOtherMessage";
 import MessageData from "../../data/MessageData.json"
+import { useEffect, useState } from "react";
 
 export default function ChatLiveFelid() {
+    const [msgData, setMsgData] = useState([]);
+    let curreentUserId = 1;
+
+    useEffect(() => {
+        setMsgData(MessageData)
+    }, [])
+
     return (
-        <ScrollView style={styles.ChatLiveFeild}>
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />            
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />     
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />     
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />     
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />     
-            <ChatLiveMyMessage  />
-            <ChatLiveOtherMessage />          
-        </ScrollView>
+        <View style={styles.ChatLiveFeild}>
+            <FlatList 
+                style={styles.list}
+                data={msgData}
+                keyExtractor={item => item.userId}
+                renderItem={({item}) =>
+                    item.userId == curreentUserId ? (
+                        <ChatLiveMyMessage />
+                    ) : (
+                        <ChatLiveOtherMessage />
+                    )
+                }
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     ChatLiveFeild: {
-        flex: 1,
         backgroundColor: "#D4E0FF",
-        flexDirection:"column",
         padding: 8,
         paddingTop: 24,
         flexGrow: 1
     },
+    list: {
+
+    }
 })
