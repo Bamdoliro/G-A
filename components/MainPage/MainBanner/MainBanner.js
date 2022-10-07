de98eb3157c467004515a9dc92e228713b31cb6e
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-07 13:42:28 +0900

    ADD :: sliderBox Style 추가 - border 추가 - DotStyle 추가

--- components/MainPage/MainBanner/MainBanner.js
+++ components/MainPage/MainBanner/MainBanner.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { SliderBox } from 'react-native-image-slider-box';
 
 function sliderTouch(index) {
     alert(index);
 }
 
-function MainBanner() {
+export default function MainBanner() {
     return(
         <View>
             <SliderBox
@@ -15,46 +15,27 @@ function MainBanner() {
                     require("../../../assets/sliderBackground.png"),
                     require("../../../assets/sliderBackground.png"),
                 ]}
+                dotColor="#fff"
                 onCurrentImagePressed = {
                     index => {
                         console.log('image pressed index : ' + index);
                         sliderTouch(index);
                     }
                 }
-                // autoplay
-                // circleLoop
+                sliderBoxHeight={130}
+                dotStyle = {{
+                    width: 13,
+                    height: 13,
+                    borderRadius: 15,
+                    marginHorizontal: 4,
+                }}
+                ImageComponentStyle={{borderRadius: 15, width: '98%', marginTop: 5}}
+                autoplay
+                circleLoop
             />
         </View>
     );
 }
 
-export default MainBanner;
-
-// let screenWidth = Dimensions.get('window').width;
-// let screenHeight = Dimensions.get('window').height / 10 * 2;
-
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       flexDirection: 'row',
-//     },
-//     bannerContent: {
-//         flex: 1,
-//         width: screenWidth,
-//         height: screenHeight,
-//         justifyContent: 'center',
-//         alignItems: 'flex-start'
-//     },
-//     titleText: {
-//         fontSize : 20,
-//         paddingHorizontal: 20,
-//         paddingBottom: 5,
-//         color: 'white',
-//         textAlign: 'center'
-//     },
-//     logoImage: {
-//         width: 60,
-//         height: 62,
-//         alignSelf: 'flex-end'
-//     }
-// });
+const styles = StyleSheet.create({
+})
