d917db3ea04dcb49d322113db16cea6691cc1fd1
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-23 22:00:24 +0900

    SET :: CommunityPostScreen 세팅

---
+++ pages/CommunityPostScreen.js
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from "react-native";
+
+export default function CommunityPostScreen() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
