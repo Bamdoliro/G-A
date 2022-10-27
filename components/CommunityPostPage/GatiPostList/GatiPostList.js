1a470e8671d938172c1f0228590b288d3f3cee2d
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-27 18:14:48 +0900

    ADD :: GatiPostList 생성

---
+++ components/CommunityPostPage/GatiPostList/GatiPostList.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+export default function GatiPostList() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
--- pages/CommunityScreen.js
+++ pages/CommunityScreen.js
@@ -1,6 +1,7 @@
 import { ScrollView, StyleSheet } from "react-native";
 import SafeAreaView from "../components/common/SafeAreaView/SafeAreaView";
-import PostList from '../components/CommunityPostPage/PostList/PostList/PostList/PostList';
+import PostList from '../components/CommunityPostPage/PostList/PostList';
+import GatiPostList from '../components/CommunityPostPage/GatiPostList/GatiPostList';
 
 export default function CommunityScreen() {
     return (
@@ -14,14 +15,7 @@ export default function CommunityScreen() {
                     ChatOfNumber={10}
                     LikeOfNumber={10}
                 />
-                <PostList 
-                    Title="세탁기 빨래 본인 것만 가져가 주세요..."
-                    Explanation="요즘에 세탁물 분실 사례가 너무 많은데 
-                    본인 빨래만 가져가고 본인 빨래가 아니다
-                    싶으면 세탁실에 다시 돌려 놓아 주세요..."
-                    ChatOfNumber={10}
-                    LikeOfNumber={10}
-                />
+                <GatiPostList />
             </ScrollView>
         </SafeAreaView>
     );
