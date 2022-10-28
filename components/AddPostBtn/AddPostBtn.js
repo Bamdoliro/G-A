8525c73f1ddda7cb167c6189e855cf8e3122e5c7
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-28 22:21:41 +0900

    SET :: AddPostBtn 세팅

---
+++ components/AddPostBtn/AddPostBtn.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function AddPostBtn() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
--- pages/CommunityScreen.js
+++ pages/CommunityScreen.js
@@ -1,10 +1,12 @@
 import {Text} from 'react-native';
+import AddPostBtn from '../components/AddPostBtn/AddPostBtn';
 import SafeAreaView from "../components/common/SafeAreaView/SafeAreaView";
 
 export default function CommunityScreen() {
     return (
         <SafeAreaView>
             <Text>CommunityScreen</Text>
+            <AddPostBtn />
         </SafeAreaView>
     );
 }
