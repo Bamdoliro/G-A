ce6069a2941d52fdc05ff25aef39e504d04141b0
seola1ne <seolalny@gmail.com>
2022-11-03 15:07:11 +0900

    MODIFY :: PostChange 버튼 useState로 수정

--- components/Community/CommunityHeader/PostChange.js
+++ components/Community/CommunityHeader/PostChange.js
@@ -1,34 +1,54 @@
 import { StyleSheet, TouchableOpacity, View, Text, useState } from 'react-native';
 
 export default function PostChange(){
-    values,
-    selectedValue={CommunityPost},
-    setSelectedValue={SetCommunityPost}
+    // values,
+    // selectedValue={CommunityPost},
+    // setSelectedValue={SetCommunityPost}
 
     const [CommunityPost, SetCommunityPost] = useState("게시글");
+
+    SetCommunityPost(() => {
+        CommunityPost = "게시글";
+    });
 
     return (
         <View style={{ flex: 1 }}>
             <View style={styles.Buttons}>
-                {values.map((value) => (
-                    <TouchableOpacity
+                    <TouchableOpacity // 게시글 버튼
                         key={value}
-                        onPress={() => setSelectedValue(value)}
+                        onPress={() => SetCommunityPost()}
                         style={[
                             styles.postChangeBtn,
-                            selectedValue === value && styles.selectedBtn,
+                            CommunityPost === "게시글" ? styles.selectedBtn : null
                         ]}
                     >
                     <Text
                         style={[
                             styles.buttonText,
-                            selectedValue === value && styles.selectedText,
+                            CommunityPost === "게시글" ? styles.selectedText : null
                         ]}
                     >
                         {value}
                     </Text>
                     </TouchableOpacity>
-                ))}
+
+                    <TouchableOpacity // 가티 버튼
+                        key={value}
+                        onPress={() => SetCommunityPost(value)}
+                        style={[
+                            styles.postChangeBtn,
+                            CommunityPost === "가티" ? styles.selectedBtn : null
+                        ]}
+                    >
+                    <Text
+                        style={[
+                            styles.buttonText,
+                            CommunityPost === "가티" ? styles.selectedText : null
+                        ]}
+                    >
+                        {value}
+                    </Text>
+                    </TouchableOpacity>
             </View>
         </View>
     );
