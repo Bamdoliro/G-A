73aacfbecd8b09997916b37c2a6ff8c78d942894
SEOKKAMONI <sj01048260279@gmail.com>
2022-11-16 15:48:48 +0900

    ADD :: map 함수로 List 나열 - PostListData Modal 추가

--- components/CommunityPostPage/ListFrame/GatiFrame.js
+++ components/CommunityPostPage/ListFrame/GatiFrame.js
@@ -1,14 +1,21 @@
 import { View, StyleSheet } from "react-native";
 import GatiPostList from "../GatiPostList/GatiPostList";
+import GatiListData from "../../../data/GatiListData.json"
 
 export default function GatiFrame() {
     return (
         <View style={styles.GatiFrame}>
-            <GatiPostList
-                Title="세탁기 빨래 본인 것만 가져가 주세요..."
-                HumanOfNumber={10}
-                Date="05/23 ~ 06/23"
-            />
+            {
+                GatiListData.map((data, index) => (
+                        <GatiPostList
+                            key={index}
+                            Title={data.title}
+                            HumanOfNumber={data.humanofnumber}
+                            Date={data.date}
+                        />
+                    )
+                )
+            }
         </View>
     )
 }
--- components/CommunityPostPage/ListFrame/PostFrame.js
+++ components/CommunityPostPage/ListFrame/PostFrame.js
@@ -1,16 +1,29 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import PostList from "../PostList/PostList";
 import PostListData from "../../../data/PostListData.json"
 
 export default function PostFrame() {
     return (
-        <View>
-            <PostList
-                    Title={PostListData.row.title}
-                    Explanation={PostListData.row.explanation}
-                    ChatOfNumber={PostListData.row.chatofnumber}
-                    LikeOfNumber={PostListData.row.likeofnumber}
-                />
+        <View style={styles.PostFrame}>
+            {
+                PostListData.map((data, index) => (
+                    <PostList
+                        key={index}
+                        Title={data.title}
+                        Explanation={data.explanation}
+                        ChatOfNumber={data.chatofnumber}
+                        LikeOfNumber={data.likeofnumber}
+                    />
+                )
+                )
+            }
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    PostFrame: {
+        width: "80%",
+        alignItems: "center"
+    }
+}) 
