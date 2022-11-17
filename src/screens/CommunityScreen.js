7f5f0d0e87ed2d5b423875327be76b12a822706b
seola1ne <seolalny@gmail.com>
2022-11-17 20:25:22 +0900

    MODIFY :: 커뮤니티 스크린 원래 코드로 수정

--- src/screens/CommunityScreen.js
+++ src/screens/CommunityScreen.js
@@ -14,7 +14,23 @@ export default function CommunityScreen({navigation}) {
 
     return (
         <SafeAreaView>
-            <CommunityReportScreen />
+            <CommunityHeader joinMembers={"125"}/>
+            <ScrollView contentContainerStyle={{alignItems: "center"}}>
+                <PostList
+                    Title="세탁기 빨래 본인 것만 가져가 주세요..."
+                    Explanation="요즘에 세탁물 분실 사례가 너무 많은데
+                    본인 빨래만 가져가고 본인 빨래가 아니다
+                    싶으면 세탁실에 다시 돌려 놓아 주세요..."
+                    ChatOfNumber={10}
+                    LikeOfNumber={10}
+                    navigation={navigation}
+                />
+                <GatiPostList
+                    Title="세탁기 빨래 본인 것만 가져가 주세요..."
+                    HumanOfNumber={10}
+                    Date="05/23 ~ 06/23"
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 }
