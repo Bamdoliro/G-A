70c0ba6710bae8e320752b8431a2ae48dc057a93
gimhanul <gimhanuly@gmail.com>
2022-11-19 12:59:56 +0900

    CHORE :: 배너 수정 (#67)
    - 라이브러리 바꿈 ㅋㅋ
    - circle loop 자연스럽게 Ok
    - banner 이미지 추가

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,45 +1,67 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { SliderBox } from 'react-native-image-slider-box';
+import React from 'react'
+import {Image, StyleSheet, View} from 'react-native'
+import Banner1 from "../../../assets/banner/banner1.png";
+import Banner2 from "../../../assets/banner/banner2.png";
+import Banner3 from "../../../assets/banner/banner3.png";
 
-function sliderTouch(index) {
-    alert(index);
-}
+import Swiper from 'react-native-swiper'
 
 export default function Banner() {
-    return(
-        <View style={styles.MainBannerContainer}>
-            <SliderBox
-                images={[
-                    require("../../../assets/sliderBackground.png"),
-                    require("../../../assets/sliderBackground.png"),
-                    require("../../../assets/sliderBackground.png"),
-                ]}
-                dotColor="#fff"
-                onCurrentImagePressed = {
-                    index => {
-                        console.log('image pressed index : ' + index);
-                        sliderTouch(index);
-                    }
-                }
-                sliderBoxHeight={130}
-                dotStyle = {{
-                    width: 13,
-                    height: 13,
-                    borderRadius: 15,
-                    marginHorizontal: 4,
-                }}
-                ImageComponentStyle={{borderRadius: 18, width: '98%', marginTop: 5}}
-                autoplay
-                autoplayInterval={15000}
-                circleLoop
-            />
+    const images = [Banner1, Banner2, Banner3];
+
+    return (
+        <View style={styles.container}>
+            <Swiper
+                containerStyle={styles.wrapper}
+                height={160}
+                autoplay={true}
+                paginationStyle={styles.paginationStyle}
+                dotStyle={styles.dot}
+                activeDotStyle={styles.activeDot}
+            >
+                {images.map((i, index) => (
+                    <View key={index}
+                          style={styles.slide}>
+                        <Image source={i} style={styles.image}/>
+                    </View>
+                ))}
+            </Swiper>
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
-    MainBannerContainer: {
-        marginBottom: 30
+    container: {
+        height: 160,
+        justifyContent: "center",
+    },
+    slide: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 30,
+    },
+    image: {
+        width: "100%",
+        height: 140,
+        borderRadius: 12,
+        borderWidth: 1,
+        borderColor: "#F6F6F6",
+    },
+    paginationStyle: {
+        bottom: 0,
+    },
+    dot: {
+        backgroundColor: '#C4C4C4',
+        width: 6,
+        height: 6,
+        borderRadius: "50%",
+        margin: 4,
+    },
+    activeDot: {
+        backgroundColor: '#0D76FF',
+        width: 6,
+        height: 6,
+        borderRadius: "50%",
+        margin: 4,
     }
 })
