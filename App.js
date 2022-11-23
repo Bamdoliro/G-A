8c552c794b9e910467e635f01343414febb6e2aa
SEOKKAMONI <sj01048260279@gmail.com>
2022-11-23 18:24:52 +0900

    ADD :: Login 준비 완료 되면 splash 사라지게 - 최소 1초는 유지하게 함

--- App.js
+++ App.js
@@ -12,11 +12,15 @@ const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient();
 
 export default function App() {
+    const [splash, setSplash] = useState(false);
     const [login, setLogin] = useState(null);
     const socket = useSocket();
 
     const setLoginToken = async () => {
         setLogin(await getAccessToken());
+        setTimeout(()=> {   
+            setSplash(true);
+        }, 1200)
     }
 
     const subscribeChat = async () => {
@@ -30,13 +34,6 @@ export default function App() {
         subscribeChat();
     }, [socket]);
 
-    const [splash, setSplash] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setSplash(true);
-        }, 1000); // 초 조절 가능
-    }, [])
 
     return (
         splash ? (
