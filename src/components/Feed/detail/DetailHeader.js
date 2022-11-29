29c8db2c6f7646e129e81cc2c1d8dac86aa98b1c
SEOKKAMONI <sj01048260279@gmail.com>
2022-11-29 21:28:02 +0900

    ADD :: Detail Header 추가

---
+++ src/components/Feed/detail/DetailHeader.js
@@ -0,0 +1,78 @@
+import { View, StyleSheet, Image,TouchableOpacity, Text } from "react-native";
+import ArrowIcon from "../../../assets/arrow.png";
+import DotDotDot from "../../../assets/dotdotdot.png"
+import Seokkamoni from "../../../assets/seokkamoni.png"
+
+export default function DetailHeader({navigation}) {
+    return (
+        <View style={styles.container}>
+            <View style={{flexDirection: "row", alignItems: "center"}}>
+                <TouchableOpacity onPress={() => navigation.pop()}>
+                    <Image
+                        source={ArrowIcon}
+                        style={styles.ArrowIcon}
+                    />
+                </TouchableOpacity>
+                <View>
+                    <Image 
+                        source={Seokkamoni}
+                        style={styles.profile}
+                    />
+                </View>
+                <View style={{paddingLeft: 5, justifyContent: "center"}}>
+                    <Text style={styles.name}>SEOKKAMONI</Text>
+                    <View style={{flexDirection: "row", alignItems: "center"}}>
+                        <View style={styles.dot} />
+                        <Text style={styles.betime}>20분 전</Text>
+                        {/* before time의 줄임말 */}
+                    </View>
+                </View>
+            </View>
+            <TouchableOpacity>
+                <Image 
+                    source={DotDotDot}
+                    style={styles.dotdotdot}
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        marginBottom: 30,
+        paddingHorizontal: 30,
+        paddingTop: 30
+    },
+    ArrowIcon: {
+        width: 10,
+        height: 20,
+        marginRight: 10
+    },
+    profile: {
+        width: 39,
+        height: 39,
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: "500",
+    },
+    betime: {
+        color: "#B0B0B0",
+        fontSize: 12,
+        paddingLeft: 2
+    },
+    dot: {
+        width: 4,
+        height: 4,
+        backgroundColor: "#B0B0B0", 
+        borderRadius: "50%",
+    },
+    dotdotdot: {
+        width: 25,
+        height: 25
+    }
+})
--- src/screens/PostDetailScreen.js
+++ src/screens/PostDetailScreen.js
@@ -1,12 +1,23 @@
+import { StyleSheet } from 'react-native';
 import SafeAreaView from '../components/common/SafeAreaView/SafeAreaView';
+import DetailHeader from '../components/Feed/detail/DetailHeader';
 
 const PostDetailScreen = ({navigation}) => {
 
     return (
-        <SafeAreaView>
-
+        <SafeAreaView style={styles.root}>
+            <DetailHeader 
+                navigation={navigation}
+                style={styles.header}
+            />
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    root: {
+        paddingHorizontal: 30
+    },
+})
 
 export default PostDetailScreen;
