3a710d0367813436944896bf660de52a2d497f74
SEOKKAMONI <sj01048260279@gmail.com>
2022-11-29 22:20:34 +0900

    ADD :: Modal Data 추가

--- src/components/Feed/detail/PostDetailComment.js
+++ src/components/Feed/detail/PostDetailComment.js
@@ -1,18 +1,23 @@
 import { Text, StyleSheet, View } from "react-native";
 import PostDetailCommentItem from "./PostDetailCommentItem";
 import Seokkamoni from "../../../assets/seokkamoni.png"
+import CommentData from "../../../data/CommentData.json" 
 
 export default function PostDetailComment() {
     return (
         <>
             <Text style={styles.title}>댓글</Text>
             <View style={styles.container}>
-                <PostDetailCommentItem 
-                    img={Seokkamoni}
-                    user="SEOKKAMONI"
-                    comment="저는 김석진입니다 하하"
-                    betime="20"
-                />
+                {
+                    CommentData.map((data) => 
+                    <PostDetailCommentItem 
+                        img={Seokkamoni}
+                        user={data.user}
+                        comment={data.comment}
+                        betime={data.betime}
+                    />
+                     )
+                }
             </View>
         </>
     )
